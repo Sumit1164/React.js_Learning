e939c8cca32a5f78b8159617c292a57713beb33a
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-14 23:40:25 +0530

    Checked and unchecked property

--- src/RadioDropdown.jsx
+++ src/RadioDropdown.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 function RadioDropdown() {
+    const [gender, setGender] = useState('male');
     return (
         <div>
             <h2>This is Radio Dropdown Component</h2>
-            <input type="radio" name="gender" id="male" value="male" /> Male
-            <input type="radio" name="gender" id="female" value="female" /> Female
+            <h4>Select your gender:</h4>
+            <input type="radio" name="gender" id="male" value="male" onChange={(event) => setGender(event.target.value)} checked={ gender=='male'} /> 
+            <label htmlFor="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female" onChange={(event)=>setGender(event.target.value)} checked={ gender=='female'}/>
+            <label htmlFor="female"> Female</label>
+            <h2>Selected Gender: { gender}</h2>
             <br />
             <br />
 
         </div>
     )
 }
+export default RadioDropdown;
