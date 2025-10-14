b6271e5a96b4b9166d8b7f618fd50c69c32e9970
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-14 23:58:13 +0530

    Use select component to list out citys

--- src/RadioDropdown.jsx
+++ src/RadioDropdown.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function RadioDropdown() {
     const [gender, setGender] = useState('male');
+    const [city, setCity] = useState('Greater Noida');
     return (
         <div>
             <h2>This is Radio Dropdown Component</h2>
@@ -13,7 +14,15 @@ function RadioDropdown() {
             <h2>Selected Gender: { gender}</h2>
             <br />
             <br />
-
+            <h3>Select City</h3>
+            <select onChange={(event) => setCity(event.target.value)} defaultValue={'Greater Noida'}>
+                <option value="noida">Noida</option>
+                <option value="Greater Noida">Greater Noida</option>
+                <option value="delhi">Delhi</option>
+                <option value="gurgaon">Gurgaon</option>
+                <option value="bangalore">Bangalore</option>
+            </select>
+            <h2>Selected City: { city}</h2>
         </div>
     )
 }
