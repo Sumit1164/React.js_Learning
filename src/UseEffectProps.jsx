75161b1ddb1930d99f2496e103f0816504506a93
Sumit tripathi <sumittripathi8795@gmail.in>
2025-10-19 15:24:07 +0530

    React Life Cycle phase

--- src/UseEffectProps.jsx
+++ src/UseEffectProps.jsx
@@ -1,13 +1,19 @@
+import { use } from "react";
 import { useEffect } from "react";
 
 const UseEffectProps = ({cnt}) => {
-    const handleCounter = () => {
-        console.log("handle counter called");
-    }
+    // const handleCounter = () => {
+    //     console.log("handle counter called");
+    // }
+    // useEffect(() => {
+    //     handleCounter();
+    // },[])
     useEffect(() => {
-        handleCounter();
-    },[])
-    
+        console.log("mounting phase only"); 
+    }, [])
+    useEffect(() => {
+        console.log("update phase only");
+    }, [cnt])
     return (
         <div>
             <h1>Use props to handle side effect</h1>
