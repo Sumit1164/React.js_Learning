5cb33398092b907692da436bd482ed157718bb2f
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-09 11:40:22 +0530

    Updating Object in State in React js

--- src/UpdatingObject.jsx
+++ src/UpdatingObject.jsx
@@ -9,12 +9,22 @@ function UpdatingObject() {
         }
     })
     const handleName = (val) => {
-        setName('Sumit Tripathi')
+        data.name = val
+        setData({...data})
+
+        // let tempData = data
+        // tempData.name = val
+        // setData({...tempData})
+    }
+    const handleCity=(val) => {
+        data.address.city = val
+        setData({...data, address:{...data.address, val}})
     }
     return (
         <div>
             <h1>Updating Object in State in React js</h1>
             <input type="text" placeholder="Update Name" onChange={(event)=>handleName(event.target.value)} />
+            <input type="text" placeholder="Update City" onChange={(event)=>handleCity(event.target.value)} />
             <h2>Name: { data.name}</h2>
             <h2>City: { data.address.city}</h2>
             <h2>Country: { data.address.country}</h2>
