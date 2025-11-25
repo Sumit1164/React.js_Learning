6c873f01cb23f38af4fdbe680a88116d15c27375
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-25 23:58:38 +0530

    Using tailwind to create an message notification

---
+++ src/TailwindCSS.jsx
@@ -0,0 +1,21 @@
+const TailwindCSS = () => {
+  return (
+    <div>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img className="size-12 shrink-0"
+          src="https://png.pngtree.com/png-clipart/20190617/original/pngtree-message-vector-icon-png-image_3876227.jpg"
+          alt="ChitChat Logo" />
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">
+            ChitChat
+          </div>
+          <p className="text-gray-500 dark:text-gray-400">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TailwindCSS
