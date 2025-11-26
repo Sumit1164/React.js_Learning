dca0b827a0b7868d7d8b397e03defe24a7cc8f95
Sumit tripathi <sumittripathi8795@gmail.com>
2025-11-26 12:52:18 +0530

    Use tailwind in react to make some mini cards and contibuter page

--- src/TailwindCSS.jsx
+++ src/TailwindCSS.jsx
@@ -1,10 +1,12 @@
 const TailwindCSS = () => {
   return (
-    <div>
+    <div style={{ margin: "20px" }}>
       <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-        <img className="size-12 shrink-0"
+        <img
+          className="size-12 shrink-0"
           src="https://png.pngtree.com/png-clipart/20190617/original/pngtree-message-vector-icon-png-image_3876227.jpg"
-          alt="ChitChat Logo" />
+          alt="ChitChat Logo"
+        />
         <div>
           <div className="text-xl font-medium text-black dark:text-white">
             ChitChat
@@ -14,6 +16,70 @@ const TailwindCSS = () => {
           </p>
         </div>
       </div>
+      <br />
+      <br />
+      <br />
+
+      <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 ...">
+        <img
+          className="mx-auto block h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src="https://static-cse.canva.com/blob/1911653/tools_transparent-background_promo-showcase_01-AFTER.jpg"
+          alt=""
+        />
+        <div className="space-y-2 text-center sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg font-semibold text-black">Erin Lindford</p>
+            <p className="font-medium text-gray-500">Product Engineer</p>
+          </div>
+          <button className="border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 ...">
+            Message
+          </button>
+        </div>
+      </div>
+      <br />
+      <br />
+      <div>
+        <div className="flex items-center space-x-2 text-base">
+          <h4 className="font-semibold text-slate-900">Contributors</h4>
+          <span className="bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700 ...">
+            204
+          </span>
+        </div>
+        <div className="mt-3 flex -space-x-2 overflow-hidden">
+          <img
+            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            className="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+        </div>
+        <div className="mt-3 text-sm font-medium">
+          <a href="#" className="text-blue-500">
+            + 198 others
+          </a>
+        </div>
+      </div>
+
+      <h2 className="bg-purple-700 text-2xl border-blue-600 border-4">Hello tailwindcss thank you</h2>
     </div>
   );
 }
